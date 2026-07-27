f69c36115412f360cb79a4225a2f8d5779cad594
vkvadrevu <vamsivadrevu22@gmail.com>
2026-07-27 08:39:46 +0000

    My second commit

---
+++ project2.py
@@ -0,0 +1,14 @@
+import os
+
+
+with open(r"C:\Users\Administrator\Desktop\servers.txt","r")as file1,open(r"C:\Users\Administrator\Desktop\report.tmp","w+")as file2:
+    for line in file1:
+        file2.write(line)
+        
+
+if os.path.isfile(r"C:\Users\Administrator\Desktop\report.txt"):
+    
+    os.rename(r"C:\Users\Administrator\Desktop\report.txt",r"C:\Users\Administrator\Desktop\report_backup.txt")
+os.rename(r"C:\Users\Administrator\Desktop\report.tmp",r"C:\Users\Administrator\Desktop\report.txt")
+   
+
