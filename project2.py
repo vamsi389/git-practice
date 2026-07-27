e6641cdb5fb760d7fef3dcc0a9096b46d4877574
vkvadrevu <vamsivadrevu22@gmail.com>
2026-07-27 08:43:28 +0000

    Modified file write operation

--- project2.py
+++ project2.py
@@ -4,6 +4,7 @@ import os
 with open(r"C:\Users\Administrator\Desktop\servers.txt","r")as file1,open(r"C:\Users\Administrator\Desktop\report.tmp","w+")as file2:
     for line in file1:
         file2.write(line)
+        file2.write(line)
         
 
 if os.path.isfile(r"C:\Users\Administrator\Desktop\report.txt"):
